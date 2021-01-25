907ed44379917fd99f3b3f9d297bd3f798a2c86a
pvn keerthi <pvnkeerthi993@gmail.com>
2021-01-25 11:49:12 -0500

    third commit

--- src/webTable/WebTable_Practice.java
+++ src/webTable/WebTable_Practice.java
@@ -1,21 +1,24 @@
 package webTable;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.sun.xml.bind.v2.schemagen.xmlschema.List;
+
 
 public class WebTable_Practice {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "E:\\Software\\chromedriver_win32\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://money.rediff.com/gainers");
-		List<> allCompanies=driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr[1]/td[1]")).getText();
+		List<WebElement> AllCompanies=driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr[1]/td[1]"));
+		System.out.println(AllCompanies.size());
 		
 	}
 
