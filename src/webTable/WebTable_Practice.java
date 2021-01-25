8fd1a6b4e904a2bce751d8c0ce24c931506009ff
pvn keerthi <pvnkeerthi993@gmail.com>
2021-01-25 11:15:58 -0500

    second comment

--- src/webTable/WebTable_Practice.java
+++ src/webTable/WebTable_Practice.java
@@ -13,7 +13,7 @@ public class WebTable_Practice {
 		System.setProperty("webdriver.chrome.driver", "E:\\Software\\chromedriver_win32\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
 		driver.get("https://money.rediff.com/gainers");
 		List<> allCompanies=driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr[1]/td[1]")).getText();
 		
